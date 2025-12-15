11391274b5572128f7f65c882876f6022aa08356
RuthuKumari <ruthukumari314@gmail.com>
2025-12-15 21:12:58 +0530

    Update App.js

--- quickserve/src/App.js
+++ quickserve/src/App.js
@@ -5,11 +5,13 @@ import Login from "./pages/login";
 import Register from "./pages/Register";
 import ServiceDetails from "./pages/ServiceDetails";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Default route → Login page */}
+        <Route path="/" element={<Login />} />
+
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -20,3 +22,4 @@ function App() {
 }
 
 export default App;
+
