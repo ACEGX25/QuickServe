f783eb136b710d86d3b57dd241c692a591f8137e
SILLYCAT <143728245+ACEGX25@users.noreply.github.com>
2026-01-25 16:31:50 +0530

    Allow Vercel Frontend via CORS

--- app/src/main/java/com/quickserve/app/config/SecurityConfig.java
+++ app/src/main/java/com/quickserve/app/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://quick-serve-lilac.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
